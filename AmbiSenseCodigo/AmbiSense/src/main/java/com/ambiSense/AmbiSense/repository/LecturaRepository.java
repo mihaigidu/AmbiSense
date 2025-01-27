246560cbabe20af4314c7918e57ff57b583ca7aa
mihaigidu <145659391+mihaigidu@users.noreply.github.com>
2025-01-27 12:00:41 +0100

    Delete AmbiSenseCodigo directory

--- AmbiSenseCodigo/AmbiSense/src/main/java/com/ambiSense/AmbiSense/repository/LecturaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ambiSense.AmbiSense.repository;
-
-import com.ambiSense.AmbiSense.model.Lectura;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LecturaRepository extends JpaRepository<Lectura, Integer> {
-}
--- AmbiSenseCodigo/AmbiSense/src/main/java/com/ambiSense/AmbiSense/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ambiSense.AmbiSense.repository;
-
-import com.ambiSense.AmbiSense.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-}
